6397d1052a037852e7c8bb7f3ed6ba5bb5a82eae
Aswin41192 <aswins41192@gmail.com>
2020-08-10 16:21:59 +0530

    Initail Commit

---
+++ src/main/java/com/tdd/example/TestController.java
@@ -0,0 +1,13 @@
+package com.tdd.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/test")
+	public String doTest() {
+		return "App Works";
+	}
+}
