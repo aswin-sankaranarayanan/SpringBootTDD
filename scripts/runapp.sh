db3821b2a44154f9196928dd3092cbfbb1c79910
aswin-sankaranarayanan <69231656+aswin-sankaranarayanan@users.noreply.github.com>
2020-08-12 15:04:01 +0530

    Updated SH file

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -1,6 +1,8 @@
+sudo su
 echo 'Starting my app'
 rm -rf '/home/ec2-user/myapp'
 mkdir '/home/ec2-user/myapp'
 cd '/home/ec2-user/myapp'
 wget https://s3-artifiact-demo.s3.amazonaws.com/target/springboot-TDD-0.0.1-SNAPSHOT.jar
-java -jar springboot-TDD-0.0.1-SNAPSHOT.jar.jar
+chmod +x springboot-TDD-0.0.1-SNAPSHOT.jar
+nohup java -jar springboot-TDD-0.0.1-SNAPSHOT.jar
