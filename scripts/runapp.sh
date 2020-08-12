4e21d1f6a019a4dd25ec48fbabba87bb5d61eb8e
aswin-sankaranarayanan <69231656+aswin-sankaranarayanan@users.noreply.github.com>
2020-08-12 16:06:47 +0530

    Sh Update

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -2,5 +2,6 @@ echo 'Starting my app'
 rm -rf '/home/ec2-user/myapp'
 mkdir '/home/ec2-user/myapp'
 cd '/home/ec2-user/myapp'
-aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar .
-java -jar ./springboot-TDD-0.0.1-SNAPSHOT.jar
+aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar springboot-TDD-0.0.1-SNAPSHOT.jar
+echo 'jar file copied'
+ls
