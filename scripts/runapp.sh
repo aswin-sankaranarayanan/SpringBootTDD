eb864455ccba2edf406ce8b08dcf8336eadba22a
Aswin41192 <aswins41192@gmail.com>
2020-08-12 16:25:54 +0530

    Config Changes

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -4,4 +4,4 @@ mkdir '/home/ec2-user/myapp'
 cd '/home/ec2-user/myapp'
 aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar springboot-TDD-0.0.1-SNAPSHOT.jar
 echo 'jar file copied'
-ls
+java -jar ./springboot-TDD-0.0.1-SNAPSHOT.jar
