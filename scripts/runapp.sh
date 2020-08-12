5a91a5437c7d3e0ab7e405430e2ad137da3fa44a
Aswin41192 <aswins41192@gmail.com>
2020-08-12 13:45:54 +0530

    shell file changes

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -1,4 +1,6 @@
 echo 'Starting my app'
+rm -rf '/home/ec2-user/myapp'
+mkdir '/home/ec2-user/myapp'
 cd '/home/ec2-user/myapp'
 wget https://s3-artifiact-demo.s3.amazonaws.com/target/springboot-TDD-0.0.1-SNAPSHOT.jar
 java -jar springboot-TDD-0.0.1-SNAPSHOT.jar.jar
