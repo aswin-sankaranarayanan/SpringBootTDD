e88c8f922a485b8f49f2ffeaee88b786535991a9
aswin-sankaranarayanan <69231656+aswin-sankaranarayanan@users.noreply.github.com>
2020-08-12 15:52:00 +0530

    Chmod removed

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -3,5 +3,4 @@ rm -rf '/home/ec2-user/myapp'
 mkdir '/home/ec2-user/myapp'
 cd '/home/ec2-user/myapp'
 aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar .
-chmod +x springboot-TDD-0.0.1-SNAPSHOT.jar
 java -jar springboot-TDD-0.0.1-SNAPSHOT.jar
