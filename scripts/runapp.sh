228e960b544c9f614c753f8aec86f190e192c37a
aswin-sankaranarayanan <69231656+aswin-sankaranarayanan@users.noreply.github.com>
2020-08-12 15:48:59 +0530

    Update runapp.sh

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -2,6 +2,6 @@ echo 'Starting my app'
 rm -rf '/home/ec2-user/myapp'
 mkdir '/home/ec2-user/myapp'
 cd '/home/ec2-user/myapp'
-wget https://s3-artifiact-demo.s3.amazonaws.com/target/springboot-TDD-0.0.1-SNAPSHOT.jar
+aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar .
 chmod +x springboot-TDD-0.0.1-SNAPSHOT.jar
 java -jar springboot-TDD-0.0.1-SNAPSHOT.jar
