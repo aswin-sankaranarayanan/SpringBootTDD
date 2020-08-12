a178f6870a73ec7490cd15412e41b767f670063c
aswin-sankaranarayanan <69231656+aswin-sankaranarayanan@users.noreply.github.com>
2020-08-12 19:10:14 +0530

    updated

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -3,5 +3,4 @@ rm -rf '/home/ec2-user/myapp'
 mkdir '/home/ec2-user/myapp'
 cd '/home/ec2-user/myapp'
 aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar springboot-TDD-0.0.1-SNAPSHOT.jar
-echo 'jar file copied'
 nohup java -jar springboot-TDD-0.0.1-SNAPSHOT.jar > server.log 2>&1 </dev/null &
