9ad21166aca87e34f44c7dbdef13e5ba77a83df3
Aswin41192 <aswins41192@gmail.com>
2020-08-12 12:25:07 +0530

    Jar Not Found Issue Fixed

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -1,3 +1,4 @@
 echo 'Starting my app'
 cd '/home/ec2-user/myapp'
+wget https://s3-artifiact-demo.s3.amazonaws.com/target/springboot-TDD-0.0.1-SNAPSHOT.jar
 java -jar springboot-TDD-0.0.1-SNAPSHOT.jar.jar
