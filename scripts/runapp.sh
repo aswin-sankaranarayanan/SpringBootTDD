3bfa6b4eacaccab10ab320a6a72fd6fb5549513f
Aswin41192 <aswins41192@gmail.com>
2020-08-13 14:50:40 +0530

    Recursive delete added

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -1,6 +1,6 @@
 echo 'Starting my app'
 pkill -f 'java -jar'
-rm -rf '/home/ec2-user/myapp'
+rm -rf '/home/ec2-user/myapp' *
 mkdir '/home/ec2-user/myapp'
 cd '/home/ec2-user/myapp'
 aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar  springboot-TDD-0.0.1-SNAPSHOT.jar
