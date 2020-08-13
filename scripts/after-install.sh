bf218db982e5a6f58cd6093db97188a2b75b98bc
Aswin41192 <aswins41192@gmail.com>
2020-08-13 17:29:51 +0530

    After Install Hook changes

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,2 +1,5 @@
 echo 'After Install'
+pkill -f 'java -jar'
+rm -rf springboot-TDD-0.0.1-SNAPSHOT.jar
+aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar  springboot-TDD-0.0.1-SNAPSHOT.jar
 chmod 777 '/home/ec2-user/myapp/springboot-TDD-0.0.1-SNAPSHOT.jar'
--- scripts/before-install.sh
+++
@@ -1,5 +0,0 @@
-echo 'Before Installing my app'
-pkill -f 'java -jar'
-cd '/home/ec2-user/myapp'
-rm -rf springboot-TDD-0.0.1-SNAPSHOT.jar
-aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar  springboot-TDD-0.0.1-SNAPSHOT.jar
