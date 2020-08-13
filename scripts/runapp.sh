1d6d2876386be5aaff6eea12fa36e19358ecb634
Aswin41192 <aswins41192@gmail.com>
2020-08-13 15:47:47 +0530

    sh changes

--- scripts/runapp.sh
+++ scripts/runapp.sh
@@ -4,5 +4,5 @@ rm -rf '/home/ec2-user/myapp' *
 mkdir '/home/ec2-user/myapp'
 cd '/home/ec2-user/myapp'
 aws s3 cp  s3://s3-artifiact-demo/target/springboot-TDD-0.0.1-SNAPSHOT.jar  springboot-TDD-0.0.1-SNAPSHOT.jar
-chmod 777 *
+chmod 777 springboot-TDD-0.0.1-SNAPSHOT.jar
 java -jar -Dspring.profiles.active=dev /home/ec2-user/myapp/springboot-TDD-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
